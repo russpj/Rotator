0fe1dac40dee67188bc94a8f0e0a015f281624bd
Russ Paul-Jones <russ@paul-jones.org>
2020-04-24 16:44:20 -0700

    Draw rectangles in different colors

--- AppMain.py
+++ AppMain.py
@@ -11,19 +11,50 @@ from kivy.clock import Clock
 
 # BoardLayout encapsulates the playing board
 class BoardLayout(BoxLayout):
-	def __init__(self):
+	def __init__(self, numCells):
 		super().__init__()
+		self.numCells = numCells
 		self.PlaceStuff()
 		self.bind(pos=self.update_rect, size=self.update_rect)
+
+	def InterpolateColor(self, beginColor, endColor, ratio):
+		betweenColor = []
+		for colorIndex in range(len(beginColor)):
+			betweenColor.append(beginColor[colorIndex]*ratio + endColor[colorIndex]*(1-ratio))
+		return betweenColor
 
 	def PlaceStuff(self):
 		with self.canvas.before:
 			Color(0.1, .3, 0.1, 1)  # green; colors range from 0-1 not 0-255
 			self.rect = Rectangle(size=self.size, pos=self.pos)
+		
+		self.rectangles = []
+		pos = [0,0]
+		posNext = [self.rect.size[0]/self.numCells, 0]
+		beginColor = [0.0, .75, .25, 1]
+		endColor = [0.0, .25, .75, 1]
+		with self.canvas:
+			for cell in range(self.numCells):
+				betweenColor = self.InterpolateColor(beginColor, endColor, float(cell)/self.numCells)
+				Color(betweenColor[0], betweenColor[1], betweenColor[2], betweenColor[3])
+				size = [posNext[0]-pos[0], self.rect.size[1]]
+				rect= Rectangle(size=size, pos=pos)
+				self.rectangles.append(rect)
+				pos = posNext.copy()
+				posNext = [self.rect.size[0]*cell/self.numCells, 0]
+			
 
 	def update_rect(self, instance, value):
 		instance.rect.pos = instance.pos
 		instance.rect.size = instance.size
+
+		numCells = len(instance.rectangles)
+		for cell in range(numCells):
+			rect = instance.rectangles[cell]
+			pos = [instance.pos[0]+instance.size[0]*cell/numCells, instance.pos[1]]
+			size = [instance.pos[0]+instance.size[0]*(cell+1)/numCells-pos[0], instance.size[1]]
+			rect.pos = pos
+			rect.size = size
 
 
 class HeaderLayout(BoxLayout):
@@ -36,11 +67,14 @@ class HeaderLayout(BoxLayout):
 		with self.canvas.before:
 			Color(0.6, .6, 0.1, 1)  # yellow; colors range from 0-1 not 0-255
 			self.rect = Rectangle(size=self.size, pos=self.pos)
+		self.statusLabel = Label(text='Ready', color = [0.7,0.05, 0.7, 1])
+		self.add_widget(self.statusLabel)
 		self.fpsLabel = Label(text='0 fps', color=[0.7, 0.05, 0.7, 1])
 		self.add_widget(self.fpsLabel)
 		
-	def UpdateText(self, fps):
-		self.fpsLabel.text = '{fpsValue:.0f} fps'.format(fpsValue=fps)
+	def UpdateText(self, fps=0, statusText='Ready'):
+		self.fpsLabel.text = '{fps:.0f} fps'.format(fps=fps)
+		self.statusLabel.text = statusText
 
 	def update_rect(self, instance, value):
 		instance.rect.pos = instance.pos
@@ -98,7 +132,7 @@ class Rotator(App):
 		layout.add_widget(self.header)
 
 		# board
-		self.boardLayout = boardLayout = BoardLayout()
+		self.boardLayout = boardLayout = BoardLayout(10)
 		layout.add_widget(boardLayout)
 
 		# footer
