d5ab5e458279fbfbbff5dede1e812ac871098f6c
Russ Paul-Jones <russ@paul-jones.org>
2020-04-25 14:10:50 -0700

    Change color underneath rectangles as programmed.
    This is also a pretty good starting point for other interactive algorithm projects.

--- AppMain.py
+++ AppMain.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
@@ -7,6 +8,34 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.clock import Clock
+
+
+class AppState(Enum):
+	Ready = 1
+	Finished = 5
+
+nextState={
+	AppState.Ready: AppState.Finished,
+	AppState.Finished: AppState.Ready
+	}
+
+class ButtonInfo:
+	def __init__(self, enabled=True, text=''):
+		self.enabled = enabled
+		self.text=text
+
+class AppInfo:
+	def __init__(self, statusText='', 
+							startInfo=ButtonInfo()):
+		self.statusText=statusText
+		self.startInfo=startInfo
+
+infoFromState = {
+	AppState.Ready: AppInfo(statusText='Ready', 
+												 startInfo=ButtonInfo(text='Reverse', enabled=True)),
+	AppState.Finished: AppInfo(statusText='Done', 
+												 startInfo=ButtonInfo(text='Reset', enabled=False))
+	}
 
 
 # BoardLayout encapsulates the playing board
@@ -29,6 +58,8 @@ class BoardLayout(BoxLayout):
 			self.rect = Rectangle(size=self.size, pos=self.pos)
 		
 		self.rectangles = []
+		self.colorValues = []
+		self.colors = []
 		pos = [0,0]
 		posNext = [self.rect.size[0]/self.numCells, 0]
 		beginColor = [0.0, .75, .25, 1]
@@ -36,14 +67,15 @@ class BoardLayout(BoxLayout):
 		with self.canvas:
 			for cell in range(self.numCells):
 				betweenColor = self.InterpolateColor(beginColor, endColor, float(cell)/self.numCells)
-				Color(betweenColor[0], betweenColor[1], betweenColor[2], betweenColor[3])
+				self.colorValues.append(betweenColor)
+				color = Color(betweenColor[0], betweenColor[1], betweenColor[2], betweenColor[3])
+				self.colors.append(color)
 				size = [posNext[0]-pos[0], self.rect.size[1]]
 				rect= Rectangle(size=size, pos=pos)
 				self.rectangles.append(rect)
 				pos = posNext.copy()
 				posNext = [self.rect.size[0]*cell/self.numCells, 0]
 			
-
 	def update_rect(self, instance, value):
 		instance.rect.pos = instance.pos
 		instance.rect.size = instance.size
@@ -55,6 +87,18 @@ class BoardLayout(BoxLayout):
 			size = [instance.pos[0]+instance.size[0]*(cell+1)/numCells-pos[0], instance.size[1]]
 			rect.pos = pos
 			rect.size = size
+
+	def ApplyColor(self, color, colorList):
+		color.r = colorList[0]
+		color.g = colorList[1]
+		color.b = colorList[2]
+		color.a = colorList[3]
+
+	def UpdateColors(self, colorIndices=None):
+		if colorIndices is not None:
+			for index in range(len(colorIndices)):
+				if index < len(self.colors):
+					self.ApplyColor(self.colors[index], self.colorValues[colorIndices[index]])
 
 
 class HeaderLayout(BoxLayout):
@@ -82,9 +126,9 @@ class HeaderLayout(BoxLayout):
 
 
 class FooterLayout(BoxLayout):
-	def __init__(self, **kwargs):
+	def __init__(self, start_button_callback=None, **kwargs):
 		super().__init__(orientation='horizontal', padding=10, **kwargs)
-		self.running = False
+		self.start_button_callback=start_button_callback
 		self.PlaceStuff()
 		self.bind(pos=self.update_rect, size=self.update_rect)
 
@@ -95,37 +139,23 @@ class FooterLayout(BoxLayout):
 		
 		self.startButton = Button(text='')
 		self.add_widget(self.startButton)
-		self.UpdateStartButtonText()
-		self.startButton.bind(on_press=self.HandleStartButton)
+		self.startButton.bind(on_press=self.start_button_callback)
+		self.UpdateButtons()
 
 	def update_rect(self, instance, value):
 		instance.rect.pos = instance.pos
 		instance.rect.size = instance.size
 
-	def HandleStartButton(self, instance):
-		self.running = not self.running
-		self.UpdateStartButtonText()
-
-	def UpdateStartButtonText(self):
-		if self.running:
-			self.startButton.text = 'Pause'
-		else:
-			self.startButton.text = 'Start'
-
-	def IsPaused(self):
-		return not self.running
-
-	def SetButtonsState(self, start_button_text):
-		if start_button_text == 'Pause':
-			self.running = True;
-		if start_button_text == 'Start':
-			self.running = False
-		self.UpdateStartButtonText()
+	def UpdateButtons(self, appInfo=infoFromState[AppState.Ready]):
+		startInfo = appInfo.startInfo
+		self.startButton.text = startInfo.text
+		self.startButton.disabled = not startInfo.enabled
 
 
 class Rotator(App):
 	def build(self):
 		self.root = layout = BoxLayout(orientation = 'vertical')
+		self.state = AppState.Ready
 
 		# header
 		self.header = HeaderLayout(size_hint=(1, .1))
@@ -136,7 +166,8 @@ class Rotator(App):
 		layout.add_widget(boardLayout)
 
 		# footer
-		self.footer = FooterLayout(size_hint=(1, .2))
+		self.footer = FooterLayout(size_hint=(1, .2), 
+														 start_button_callback=self.StartButtonCallback)
 		layout.add_widget(self.footer)
 
 		# self.solver = SudokuSolver(easyBoard, yieldLevel=0)
@@ -153,20 +184,26 @@ class Rotator(App):
 			fpsValue = 1/dt
 		else:
 			fpsValue = 0
-		if self.footer.IsPaused():
+		if self.state==AppState.Finished:
 			return
 
 		try:
 			# result = next(self.generator)
-			self.UpdateText(fps=fpsValue)
+			self.UpdateUX(fps=fpsValue, state=self.state)
 		except StopIteration:
 			# kill the timer
-			self.UpdateText(fps=fpsValue, updatePositions = self.footer.IsPaused)
-			self.footer.SetButtonsState(start_button_text = 'Start')
-			
+			self.UpdateUX(fps=fpsValue, state=self.state)
 
-	def UpdateText(self, fps, updatePositions = True):
+	def UpdateUX(self, fps=0, state=AppState.Ready):
+		appInfo = infoFromState[self.state]
 		self.header.UpdateText(fps = fps)
+		self.footer.UpdateButtons(appInfo=appInfo)
+
+	def StartButtonCallback(self, instance):
+		self.state = nextState[self.state]
+		self.UpdateUX(state=self.state)
+		self.boardLayout.UpdateColors([2, 3, 4 ,5, 6, 7, 8, 9, 0, 1])
+
 
 def Main():
 	Rotator().run()
